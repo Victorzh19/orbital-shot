61788ac9690f674a6e18be807b4a91ca9658960c
Carlos <coellocarlos132@gmail.com>
2026-09-23 00:12:13 -0500

    1

--- kerbin_we_going_to_mars.py
+++ kerbin_we_going_to_mars.py
@@ -48,11 +48,5 @@ Apollo_Negative_11 = conn.space_center.active_vessel
 
 
 
-#================================= test ===============================================
 
-print("Okkkkkk lets go")
-Apollo_Negative_11.control.throttle = 1.0
-Apollo_Negative_11.control.activate_next_stage()
-time.sleep(3)
-print("I think its working")
 
