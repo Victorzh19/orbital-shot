4e336aa038b5a822c26ce926a4b2db5ffa392afd
Carlos <coellocarlos132@gmail.com>
2026-09-22 23:55:15 -0500

    This is the first change to the backbone of the code

--- Lunch test.py
+++ Lunch test.py
@@ -1,14 +1,56 @@
 
 import krpc
+import time
+import math
 
+
+
+
+
+
+#========================= connection sequence ======================
 conn = krpc.connect(
     name="Launch test",
     address="100.85.76.35",
 )
 Apollo_Negative_11 = conn.space_center.active_vessel
+
+#================================= Main ===============================================
 print("Okkkkkk lets go")
 Apollo_Negative_11.control.throttle = 1.0
 Apollo_Negative_11.control.activate_next_stage()
+time.sleep(3)
 print("I think its working")
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#================================= test ===============================================
