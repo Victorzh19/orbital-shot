4842492d0fc3280b1319ea4ac8ed8073b4e1e9af
Carlos <coellocarlos132@gmail.com>
2026-09-22 23:28:14 -0500

    This was the renaming of the vessel

--- Lunch test.py
+++ Lunch test.py
@@ -1,13 +1,14 @@
-import time
+
 import krpc
 
 conn = krpc.connect(
     name="Launch test",
     address="100.85.76.35",
 )
-vessel = conn.space_center.active_vessel
+Apollo_Negative_11 = conn.space_center.active_vessel
 print("Okkkkkk lets go")
-vessel.control.throttle = 1.0
-vessel.control.activate_next_stage()
+Apollo_Negative_11.control.throttle = 1.0
+Apollo_Negative_11.control.activate_next_stage()
 print("I think its working")
+
 
